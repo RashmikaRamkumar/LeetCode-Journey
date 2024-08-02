2039debaaa42d16a5b20a86225391ec2789aecfc
Rashmika K R <krrashmika2004@gmail.com>
2024-08-02 09:57:22 +0000

    [LeetCode Sync] Runtime - 1 ms (99.49%), Memory - 44.9 MB (87.24%)

--- 0412-fizz-buzz/solution.java
+++ 0412-fizz-buzz/solution.java
@@ -1,21 +1,24 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
-        List<String> result=new ArrayList<>();
+        List<String> output=new ArrayList<String>(); 
         int i;
         for(i=1;i<=n;i++)
         {
             if(i%3==0 && i%5==0)
-                result.add("FizzBuzz");
+                output.add("FizzBuzz");
             else if(i%3==0)
-                result.add("Fizz");
+                output.add("Fizz");
             else if(i%5==0)
-                result.add("Buzz");
+                output.add("Buzz");
             else
-                result.add(Integer.toString(i));
-            
-        }
-       return result;
- 
-    }
+            {
+              String a = Integer.toString(i);
+              output.add(a);
 
+            }
+                
+        } 
+        return output;
+
+    }
 }
