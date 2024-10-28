e99e46a03fa0d4760ca4a59a31c31ff540776094
Rashmika K R <krrashmika2004@gmail.com>
2024-10-28 15:47:25 +0000

    [LeetCode Sync] Runtime - 777 ms (67.25%), Memory - 0B (100.00%)

--- 2087-confirmation-rate/solution.sql
+++ 2087-confirmation-rate/solution.sql
@@ -1,4 +1,20 @@
-select s.user_id, round(avg(if(c.action="confirmed",1,0)),2) as confirmation_rate
-from Signups as s left join Confirmations as c on s.user_id= c.user_id group by user_id;
-
+WITH ConfirmationCounts AS (
+    SELECT 
+        user_id,
+        COUNT(*) AS total_requests,
+        SUM(CASE WHEN action = 'confirmed' THEN 1 ELSE 0 END) AS confirmed_requests
+    FROM Confirmations
+    GROUP BY user_id
+),
+AllUsers AS (
+    SELECT s.user_id
+    FROM Signups s
+    LEFT JOIN Confirmations c ON s.user_id = c.user_id
+    GROUP BY s.user_id
+)
+SELECT 
+    u.user_id,
+    ROUND(COALESCE(1.0 * confirmed_requests / total_requests, 0), 2) AS confirmation_rate
+FROM AllUsers u
+LEFT JOIN ConfirmationCounts cc ON u.user_id = cc.user_id;
 
