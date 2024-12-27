194db23eff31ec40d6f63d04e607bb1d90633cf4
Rashmika K R <krrashmika2004@gmail.com>
2024-12-27 18:33:08 +0000

    [LeetCode Sync] Runtime - 160 ms (8.87%), Memory - 45.4 MB (18.22%)

--- 0018-4sum/solution.java
+++ 0018-4sum/solution.java
@@ -1,41 +1,34 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> ans = new ArrayList<>();
-        int len = nums.length;
-
         Arrays.sort(nums);
-
-        for (int i = 0; i < len - 3; i++) {
-            if (i > 0 && nums[i - 1] == nums[i]) 
-                continue;
-
-            for (int j = i + 1; j < len - 2; j++) {
-                if (j > i + 1 && nums[j - 1] == nums[j])
-                    continue;
-                
-                int k = j + 1;
-                int l = len - 1;
-
-                while (k < l) {
-                    long sum = nums[i] + nums[j];
-                    sum += nums[k] + nums[l];
-
-                    if (sum == target) {
-                        ans.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
-                        k++;
-                        l--;
-
-                        while (k < l && nums[k - 1] == nums[k]) k++;
-                        
-                        while (k < l && nums[l + 1] == nums[l]) l--;
-                    } else if (sum < target)
-                        k++;
-                    else 
-                        l--;
+        Set<List<Integer>> result = new HashSet<>();
+        int i,j,k,l;
+        int n=nums.length;
+        for(i=0;i<n;i++)
+        {
+            for(j=i+1;j<n-1;j++)
+            {
+                k=j+1;
+                l=n-1;
+                while(k<l){
+                long sum=(long) nums[i]+nums[j]+nums[k]+nums[l];
+                if(sum==target)
+                {
+                    result.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
+                    k++;
+                    l--;
+                }
+                else if(sum<target)
+                    k++;
+                else if(sum>target)
+                    l--;
                 }
             }
         }
-
+        ans.addAll(result);
         return ans;
+
+        
     }
 }
