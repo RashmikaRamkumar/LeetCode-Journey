00ed84b674c20beb3f3ebd5ff3fba6fd872b9190
Rashmika K R <krrashmika2004@gmail.com>
2024-12-29 13:08:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 57.7 MB (12.89%)

--- 0189-rotate-array/solution.java
+++ 0189-rotate-array/solution.java
@@ -1,15 +1,21 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int n = nums.length;
-        k = k % n;
-        int[] rotated = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            rotated[(i + k) % n] = nums[i];
-        }
-
-        for (int i = 0; i < n; i++) {
-            nums[i] = rotated[i];
-        }        
+        int n=nums.length;
+        k=k%n;
+        reverse(nums,0,n-k-1);
+        reverse(nums,n-k,n-1);
+        reverse(nums,0,n-1);
     }
+    public static void reverse(int[] nums,int strt,int end)
+        {
+            int temp;
+            while(strt<end)
+            {
+                temp=nums[strt];
+                nums[strt]=nums[end];
+                nums[end]=temp;
+                strt++;
+                end--;
+            }
+        }
 }
