70f72ae83dc0536ddc75d89c2e19807c979c56f9
Rashmika K R <krrashmika2004@gmail.com>
2025-01-06 18:07:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44 MB (64.48%)

--- 0237-delete-node-in-a-linked-list/solution.java
+++ 0237-delete-node-in-a-linked-list/solution.java
@@ -10,5 +10,7 @@ class Solution {
     public void deleteNode(ListNode node) {
         node.val=node.next.val;
         node.next=node.next.next;
+        //dont think interms of addreass as we need head to acess or traverse the linked list intems of address and we only have the node whcih is to be deleted
+        //we can simply do it by assigning the value of the next node it and as well as chanign the address to point the second next element from here
     }
 }
