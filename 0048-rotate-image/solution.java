cdf00e999d5e952e6eba0ff8a07ad1d105ed13c6
Rashmika K R <krrashmika2004@gmail.com>
2025-01-20 17:50:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42 MB (85.64%)

--- 0048-rotate-image/solution.java
+++ 0048-rotate-image/solution.java
@@ -1,33 +1,35 @@
 class Solution {
-    public void rotate(int[][] matrix) {
-        transpose(matrix);
-        reverse_row(matrix);
-        
+    public int[][] transpose(int[][] matrix)
+    {
+
+        for(int i=0;i<matrix.length;i++)
+        {
+            for(int j=i+1;j<matrix.length;j++)
+            {
+                int temp;
+                temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        return matrix;        
     }
-    public static void transpose(int[][] matrix)
+    public void row_rev(int[][] matrix)
+    {
+        int n=matrix.length;
+        for(int i=0;i<matrix.length;i++)
         {
-            int i,j,n=matrix.length;
-            for(i=0;i<n;i++)
+            for(int j=0;j<matrix.length/2;j++)
             {
-                for(j=i+1;j<n;j++)
-                {
-                    int temp=matrix[i][j];
-                    matrix[i][j]=matrix[j][i];
-                    matrix[j][i]=temp;
-                }
+                int temp;
+                temp=matrix[i][j];
+                matrix[i][j]=matrix[i][n-j-1];
+                matrix[i][n-j-1]=temp;
             }
         }
-        public static void reverse_row(int[][] matrix)
-        {
-            int i,j,n=matrix.length;
-            for(i=0;i<n;i++)
-            {
-                for(j=0;j<n/2;j++)
-                {
-                    int temp=matrix[i][j];
-                    matrix[i][j]=matrix[i][n-j-1];
-                    matrix[i][n-j-1]=temp;
-                }
-            }
-        }
+    }
+    public void rotate(int[][] matrix) {
+        
+        row_rev(transpose(matrix));
+    }
 }
