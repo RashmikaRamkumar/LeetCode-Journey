d878a981c72e436d85482bbb9adc26d83cec386d
Rashmika K R <krrashmika2004@gmail.com>
2025-01-21 14:20:56 +0000

    [LeetCode Sync] Runtime - 2 ms (94.06%), Memory - 133.5 MB (89.01%)

--- 0334-increasing-triplet-subsequence/solution.java
+++ 0334-increasing-triplet-subsequence/solution.java
@@ -1,12 +1,16 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-       int min1 = Integer.MAX_VALUE;
-       int min2 = Integer.MAX_VALUE;
-       for(int n : nums) {
-           if(n <= min1) min1 = n;
-           else if(n <= min2) min2 = n;
-           else return true;
-       }
-       return false;
+        int i=Integer.MAX_VALUE,j=Integer.MAX_VALUE;
+        for(int num : nums)
+        {
+            if(num<=i)
+                i=num; // if so found the first small number
+            else if(num<=j)
+                j=num; 
+                // if so found the second smallest num i.e which u\is greater than our current first small
+            else
+                return true; // if its greater than first and second it seem to be the expected triplet sequence
+        }
+        return false; // if such sequence is not met in running the whole loop then we should return false;
     }
 }
