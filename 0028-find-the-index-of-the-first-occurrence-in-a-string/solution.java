0cc9f27057386fa5478f60be623797a6ca8545fd
Rashmika K R <krrashmika2004@gmail.com>
2025-01-28 17:05:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.6 MB (63.89%)

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/solution.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/solution.java
@@ -1,5 +1,18 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+        int h=haystack.length();
+        int n=needle.length();
+        int i=0,j=0;
+        for(i=0;i<=h-n;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                if(haystack.charAt(i+j)!=needle.charAt(j))
+                    break;
+            }
+            if(j==n)
+                return i;
+        }
+        return -1;
     }
 }
