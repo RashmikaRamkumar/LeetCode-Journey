05bdf4044e7e7b6a564c56993f49f5126c2e5e69
Rashmika K R <krrashmika2004@gmail.com>
2025-03-12 06:29:12 +0000

    [LeetCode Sync] Runtime - 1 ms (82.65%), Memory - 44.3 MB (21.43%)

---
+++ 2551-apply-operations-to-an-array/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        
+        for(int i=0;i<nums.length-1;i++)
+        {
+            if(nums[i]==nums[i+1]){
+                nums[i]=nums[i]*2;
+                nums[i+1]=0;
+            }
+
+        }
+        int res[]=new int[nums.length];
+        int i=0;
+        for(int num:nums)
+        {
+            if(num!=0)
+            res[i++]=num;
+
+        }
+        return res;
+    }
+}
