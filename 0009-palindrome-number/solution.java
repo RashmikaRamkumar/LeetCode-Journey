14dab03df09ca47aa322cb5f923e5c987df1fc81
Rashmika K R <krrashmika2004@gmail.com>
2025-04-27 11:10:04 +0000

    [LeetCode Sync] Runtime - 5 ms (71.27%), Memory - 44.2 MB (47.96%)

--- 0009-palindrome-number/solution.java
+++ 0009-palindrome-number/solution.java
@@ -1,21 +1,12 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x < 0 ||(x!=0 && x%10==0 ))
-         // negative num and if num last digit it zero only if num itself is not zerp
-            return false;
-        //int temp=x;
-        int rev=0;
-        
-        while(x>rev)
-        {
-            //reversing a number logic applied only for the second half of the number
-            rev=rev*10 + x % 10;
-            x/=10;
-        }
-        //x has the first half and rev has the seconf half(reversed)
-        //if the number is odd-length (in which case we can safely ignore the middle digit 
-        // if 121 both half has 2 we simply ignore 2 and compare others in case of oddDigit num
-
-        return (x==rev) || (x==rev/10);
+       int temp=x,res=0;;
+       while (x>0)
+       {
+        int digit=x%10;
+        res=res*10+digit;
+        x/=10;
+       }
+       return res==temp;
     }
 }
