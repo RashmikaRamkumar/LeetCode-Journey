1151da751b68ddaf3282855367cefecb2b48ba7a
Rashmika K R <krrashmika2004@gmail.com>
2025-04-27 07:24:40 +0000

    [LeetCode Sync] Runtime - 1 ms (86.82%), Memory - 41 MB (79.38%)

---
+++ 2715-k-items-with-the-maximum-sum/solution.java
@@ -0,0 +1,5 @@
+class Solution {
+    public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        return (k<=numOnes) ? k : (k<=(numOnes+numZeros)) ? numOnes : (numOnes-(k-(numOnes+numZeros)));
+    }
+}
