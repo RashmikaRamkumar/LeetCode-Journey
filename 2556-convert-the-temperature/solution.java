9cd66af3d663ffd369a4223770ed6a28abbd2ace
Rashmika K R <krrashmika2004@gmail.com>
2025-04-27 12:00:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.1 MB (42.56%)

--- 2556-convert-the-temperature/solution.java
+++ 2556-convert-the-temperature/solution.java
@@ -1,10 +1,7 @@
 class Solution {
     public double[] convertTemperature(double celsius) {
-        double fahrenheit=(celsius*(1.80)+32);
-        double kelvin=celsius +273.15;
-        double ans[]=new double[2];
-        ans[0]=kelvin;
-        ans[1]=fahrenheit;
-        return ans;
+        double k=celsius+273.15;
+        double f=celsius*1.80+32.00;
+        return new double[]{k,f};
     }
 }
