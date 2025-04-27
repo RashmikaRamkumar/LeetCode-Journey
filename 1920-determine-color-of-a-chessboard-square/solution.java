b7c4010206a203cbe0e08b7209d2a7a6846f9679
Rashmika K R <krrashmika2004@gmail.com>
2025-04-27 18:51:06 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.5 MB (47.62%)

--- 1920-determine-color-of-a-chessboard-square/solution.java
+++ 1920-determine-color-of-a-chessboard-square/solution.java
@@ -1,14 +1,10 @@
 class Solution {
     public boolean squareIsWhite(String coordinates) {
-        char letter=coordinates.charAt(0);
-        if(letter =='a'||letter =='c'||letter =='e'||letter =='g')
-            letter='1';
-        else
-            letter='2';
-        System.out.println(letter);
-        int num=coordinates.charAt(1)-'0';
-        if((letter=='1' && num%2!=0) ||(letter=='2' && num%2==0))
-            return false;
-        return true;
+        char a=coordinates.charAt(0);
+        int b=coordinates.charAt(1)-'0';
+        int c=a-'a';
+        if((c%2==0 && b%2==0) ||(c%2!=0 && b%2!=0))
+            return true;
+        return false;
     }
 }
