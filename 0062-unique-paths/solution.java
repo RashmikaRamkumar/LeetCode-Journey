fa5ec8dd2a794c368e4079a1f9a91bfd4f9fae1a
Rashmika K R <krrashmika2004@gmail.com>
2025-07-11 06:26:57 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.3 MB (89.02%)

--- 0062-unique-paths/solution.java
+++ 0062-unique-paths/solution.java
@@ -1,24 +1,12 @@
 class Solution {
-    public int solve(int i,int j,int m ,int n,int[][] dp)
-    {
-        if(i==m-1 && j==n-1)
-            return 1;
-        if (i>=m || j>=n)
-            return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        else
-            dp[i][j]=solve(i+1,j,m,n,dp)+solve(i,j+1,m,n,dp);
-            return dp[i][j];
-    }
     public int uniquePaths(int m, int n) {
-        int[][] dp=new int[m+1][n+1];
-        // Arrays.fill(dp,-1);
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
+        int N=m+n-2;
+        long res=1;
+        int r=Math.min(m-1,n-1);
+        for(int i=1;i<=r;i++)
+        {
+            res=res*(N-r+i)/i;
         }
-        return solve(0,0,m,n,dp);
-        
-        
+        return (int)res;
     }
 }
