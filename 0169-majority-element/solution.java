377149ab7b3e1e15eaca430b792205009cd68b97
Rashmika K R <krrashmika2004@gmail.com>
2025-07-18 14:46:14 +0000

    [LeetCode Sync] Runtime - 1 ms (99.86%), Memory - 53 MB (66.72%)

--- 0169-majority-element/solution.java
+++ 0169-majority-element/solution.java
@@ -1,24 +1,17 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> occur=new HashMap<Integer,Integer>();
-        for(int i: nums)
-        {
-            if(occur.containsKey(i))
-                occur.put(i,occur.get(i)+1);
-            else
-                occur.put(i,1);
-        }
         int n=nums.length;
-        int r=0;
-        for(int i:nums)
+        int count=0;
+        int curr=0;
+        for(int i=0;i<n;i++)
         {
-            if(occur.get(i)>(n/2))
-            {
-                r=i;
-                break;
-            }
-
+            if(count==0)
+                curr=nums[i];
+            if(curr==nums[i])
+                count+=1;
+            else
+                count--;
         }
-        return r;
+        return curr;
     }
 }
